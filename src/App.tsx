0d33b3e516b4d8dac27b83c51af3cf6aeae0d648
Brian Ingles <github@emeraldwalk.com>
2024-10-17 08:35:36 -0500

    Checklist example

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,22 @@ import type { Component } from 'solid-js'
 import styles from './App.module.css'
 import RichTextExample from './examples/richText'
 import { ExampleContent } from './examples/components/exampleContent'
-import { Route, Router } from '@solidjs/router'
+import { Route, Router, useNavigate } from '@solidjs/router'
+import CheckListsExample from './examples/checkLists'
+
+function RedirectToDefault() {
+  const navigate = useNavigate()
+  navigate('/rich-text')
+  return null
+}
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
       <Router base={import.meta.env.BASE_URL} root={ExampleContent}>
-        <Route path="/" component={RichTextExample} />
-        <Route path="*404" component={() => <div>Not found.</div>} />
+        <Route path="/check-lists" component={CheckListsExample} />
+        <Route path="/rich-text" component={RichTextExample} />
+        <Route path="*404" component={RedirectToDefault} />
       </Router>
     </div>
   )
