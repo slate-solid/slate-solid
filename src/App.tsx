7c5c644739211271f77d81567279a82236d5e43b
Brian Ingles <github@emeraldwalk.com>
2024-10-18 22:30:20 -0500

    Removed test icons

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,10 @@ import styles from './App.module.css'
 import { Router } from '@solidjs/router'
 import { Layout } from './examples/components/layout'
 import { routes } from './examples/routes'
-import { SvgLetterIcon } from './examples/components/svgLetterIcon'
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
-      <SvgLetterIcon bg="#477bbe" fg="#fff">
-        S
-      </SvgLetterIcon>
-      <SvgLetterIcon bg="#d8d8d8" fg="#505050">
-        S
-      </SvgLetterIcon>
       <Router base={import.meta.env.BASE_URL} root={Layout}>
         {routes}
       </Router>
