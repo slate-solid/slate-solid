0f63044fbce022aba6c4611824280b4fda52e9e2
Brian Ingles <github@emeraldwalk.com>
2024-10-31 19:35:38 -0500

    isJsonStringEqual

--- packages/core/src/utils/isEqual.ts
+++ packages/core/src/utils/isEqual.ts
@@ -5,3 +5,10 @@
 export function isArrayEqual<T>(a: T[], b: T[]) {
   return a.length === b.length || a === b
 }
+
+/**
+ * Compare JSON.stringify values of 2 objects and return true if equal.
+ */
+export function isJsonStringEqual<T>(a: T, b: T) {
+  return JSON.stringify(a) === JSON.stringify(b)
+}
