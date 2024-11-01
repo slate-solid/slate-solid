29b9ae62836554013f73596253c7fab393c1c010
Brian Ingles <github@emeraldwalk.com>
2024-10-31 19:24:02 -0500

    Comment

--- packages/core/src/utils/isArrayEqual.ts
+++ packages/core/src/utils/isArrayEqual.ts
@@ -1,3 +1,7 @@
+/**
+ * Slight optimization such that empty arrays match regardless of reference
+ * equality.
+ */
 export function isArrayEqual<T>(a: T[], b: T[]) {
   return a.length === b.length || a === b
 }
