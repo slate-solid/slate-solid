2c1eecc6049c758544471d1146e74a9ec936bcbe
Brian Ingles <github@emeraldwalk.com>
2024-11-04 22:28:33 -0600

    Handle cases where children ref has changed when `set_selection` operation is being handled

--- packages/core/src/utils/weakMaps.ts
+++ packages/core/src/utils/weakMaps.ts
@@ -1,4 +1,6 @@
-import type { Node } from 'slate'
+import type { Descendant, Editor, Node } from 'slate'
+
+export const EDITOR_TO_CHILDREN = new WeakMap<Editor, Descendant[]>()
 
 /** Stores paths of nodes */
 export const NODE_TO_PATH: WeakMap<Node, number[]> = new WeakMap()
